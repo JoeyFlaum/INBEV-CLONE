145cd4d0919a6b7dec94d39d7774c4a4c77cee94
JoeyFlaum <58762590+Therealaveragejoe@users.noreply.github.com>
2021-07-13 00:18:00 -0500

    layout of ID page 80% complete

--- src/Components/IdPage.js
+++ src/Components/IdPage.js
@@ -1,28 +1,35 @@
+import './IdPage.css'
+
 function IdPage() {
   return (
     <main id="id-page">
+      <div className = 'background-image'></div>
+      <div className = 'id-wrapper'>
       <h1>Can we see some ID?</h1>
       <p>
         As part of our commitment to responsible drinking,
         <br />
         please confirm you are of legal drinking age.
       </p>
-      <form>
-        <select className="locations">
+      <form onSubmit={(e)=>e.preventDefault()}>
+        <select name = "country" className="locations">
           <option value="null">Location</option>
         </select>
-        <select id="month" className="DOB">
+        <div className = 'dob-wrapper'>
+        <select name = "month" id="month" className="DOB">
           <option value="null">Month</option>
         </select>
-        <select id="day" className="DOB">
+        <select name = "day" id="day" className="DOB">
           <option value="null">Day</option>
         </select>
-        <select id="year" className="DOB">
+        <select name = "year" id="year" className="DOB">
           <option value="null">Year</option>
         </select>
-        <button className="enter-site">Enter</button>
-        <p>Enjoy Responsibly</p>
+        </div>
+        <button className="enter-site">ENTER</button>
       </form>
+        <p>Enjoy Responsibly</p>
+      </div>
     </main>
   );
 }
