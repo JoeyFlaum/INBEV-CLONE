9fa5503ed1885169c6318ff4a72c5a43f7cd4278
JoeyFlaum <58762590+Therealaveragejoe@users.noreply.github.com>
2021-08-10 16:11:41 -0500

    in process laying out header

--- src/Components/ColorStrip/ColorStrip.js
+++ src/Components/ColorStrip/ColorStrip.js
@@ -1,9 +1,12 @@
-export default function ColorStrip({additionalClassName}){
-    return(
-        <div className={`color-strip ${additionalClassName}`} style ={{
-            height:"4px",
-            width: "100vw",
-            backgroundImage: `linear-gradient(to right,rgb(180, 38, 2),#f49c00)`,
-        }}></div>
-    )
+export default function ColorStrip({ additionalClassName }) {
+  return (
+    <div
+      className={`color-strip ${additionalClassName}`}
+      style={{
+        height: "4px",
+        width: "100vw",
+        backgroundImage: `linear-gradient(to right,rgb(180, 38, 2),#f49c00)`,
+      }}
+    ></div>
+  );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,62 @@
-
-
-export default function Header(){
-    return(
-        <>
-        <div>Header</div>
-        </>
-    )
+export default function Header() {
+  const links = {
+    tabs: [
+      {
+        mainTab: { title: "WHO WE ARE", link: "#" },
+        subTabs: [
+          { title: "Our Colleagues", link: "#" },
+          { title: "Our Leaders", link: "#" },
+          {
+            title: "Our Vision",
+            link: "#",
+            subTabs: [
+              { title: "Our Values", link: "#" },
+              { title: "Manifesto", link: "#" },
+            ],
+          },
+          { title: "Diversity & Inclusion", link: "#" },
+          { title: "Heritage", link: "#" },
+        ],
+      },
+      {
+        mainTab: { title: "WHAT WE DO", link: "#" },
+        subTabs: [
+          { title: "Beer & Brewing", link: "#" },
+          { title: "Innovation", link: "#" },
+          { title: "Road Safety", link: "#" },
+        ],
+      },
+      {mainTab: { title: "OUR BRANDS", link: "#" },//TODO: COMPLETE SUBTABS
+      subTabs: []},
+      {mainTab: { title: "OUR POLICIES", link: "#" },
+      subTabs: []},
+      {mainTab: { title: "SMART DRINKING", link: "#" },
+      subTabs: []},
+      {mainTab: { title: "SUSTAINABILITY", link: "#" },
+      subTabs: []},
+      {mainTab: { title: "INVESTORS", link: "#" },
+      subTabs: []},
+      {mainTab: { title: "CAREERS", link: "#" },
+      subTabs: []},
+      {mainTab: { title: "SUPPLIERS", link: "#" },
+      subTabs: []},
+      {mainTab: { title: "NEWS & MEDIA", link: "#" },
+      subTabs: []},
+    ],
+  };
+  return (
+    <header>
+      <h1>
+        <a href="/" rel="home">
+          <img
+            src="https://www.ab-inbev.com/etc.clientlibs/abinbev/clientlibs/clientlib-site/resources/brand-assets/abinbev_logo_en.svg"
+            alt="AB Inbev logo"
+          />
+        </a>
+      </h1>
+      <nav>
+        <ul></ul>
+      </nav>
+    </header>
+  );
 }
