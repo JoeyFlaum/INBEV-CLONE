80f6b16f915f95c47d860798643a30ddac6a2dc5
JoeyFlaum <58762590+Therealaveragejoe@users.noreply.github.com>
2021-09-02 15:18:35 -0500

    complete mission section

--- src/Components/Mission/Mission.js
+++ src/Components/Mission/Mission.js
@@ -1,10 +1,18 @@
 import ColorStrip from "../ColorStrip/ColorStrip";
-
+import "./Mission.css"
 export default function Mission() {
   return (
-    <>
-      <ColorStrip additionalClassName = "misson-divider" />
-      <div>Mission</div>
-    </>
+    <section className = "mission__section" >
+      <ColorStrip additionalClassName="misson-divider" />
+      <div className = "mission__staement-wrapper">
+      <p className = "mission__statement">
+        Building a better world is a task that can only be achieved when we work
+        together, even when we’re at a distance. We’re building a company to
+        last — brewing beer and building brands that will continue to bring
+        people together for the next 100 years and beyond. We are committed to
+        working with our partners towards a more sustainable future.
+      </p>
+      </div>
+    </section>
   );
 }
