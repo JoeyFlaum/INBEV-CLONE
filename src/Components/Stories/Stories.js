54497f3696488a6fe8504863cc8f836f4ad660cd
JoeyFlaum <58762590+Therealaveragejoe@users.noreply.github.com>
2021-10-16 01:41:56 -0500

    change npm carousel to slick slider. Slider is working. Needs layout tweaks

--- src/Components/Stories/Stories.js
+++ src/Components/Stories/Stories.js
@@ -2,59 +2,36 @@ import NewsCard from "../NewsCard/NewsCard";
 import Social from "../Social";
 import "./Stories.css";
 import ShareArrow from "../../Assets/ShareArrow";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-const content = [
-  {
-    title: "INNOVATION",
-    image:
-      "https://www.ab-inbev.com/content/dam/universaltemplate/ab-inbev/News/Stories/esports-how-ab-inbev-is-capturing-one-of-the-worlds-fastest-growing-entertainment-segments/ESports_427x248.jpg",
-    snippet:
-      "Esports: How AB InBev is Capturing One of the World’s Fastest Growing Entertainment Segments",
-    alt: "Sports Drinks",
-    href: "#",
-    date: "September 07, 2021",
-  },
-  {
-    title: "WATER STEWARDSHIP",
-    image:
-      "https://www.ab-inbev.com/content/dam/universaltemplate/ab-inbev/News/Stories/suma-nativas-working-together-we-can-restore-what-was-destroyed/SumaNativas_427x248.jpg",
-    snippet:
-      "Sumá Nativas: “Working together we can restore what was destroyed”",
-    alt: "Man with plants",
-    href: "#",
-    date: "August 30, 2021",
-  },
-  {
-    title: "BEER & BREWING",
-    image:
-      "	https://www.ab-inbev.com/content/dam/universaltemplate/ab-inbev/News/Stories/bavaria-launches-nativa-a-cassava-based-beer-grown-in-the-colombian-caribbean-/_Nativa_427x248.jpg",
-    snippet:
-      "Bavaria launches Nativa, a cassava-based beer grown in the Colombian Caribbean",
-    alt: "Beer with vegetables in a basket",
-    href: "#",
-    date: "August 23, 2021",
-  },
-  {
-    title: "DREAM PEOPLE CULTURE",
-    image:
-      "https://www.ab-inbev.com/content/dam/universaltemplate/ab-inbev/News/Stories/our-commitment-to-a-better-and-equal-world-for-all--/EqualWorld_427x248.jpg",
-    snippet: "Our Commitment to a Better and Equal World for All",
-    alt: "Our Commitment to a Better and Equal World for All",
-    href: "#",
-    date: "June 23, 2020",
-  },
-  {
-    title: "OUR STORIES",
-    image:
-      "https://www.ab-inbev.com/content/dam/universaltemplate/ab-inbev/News/Stories/our-response-to-covid19-making-an-impact-where-it-matters/OurGlobalResponse_427x248.jpg",
-    snippet: "Our response to COVID19: Making an impact where it matters",
-    alt: "black and white portraits of faces in a grid",
-    href: "#",
-    date: "April 08, 2020",
-  },
-];
-export default function Stories({windowWidth}) {
+import { content } from "./StoriesContent";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block",bottom:"-30px",top:"unset", right:"25%", background: "red",zIndex:"2"}}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block",bottom:"-30px",top:"unset", right:"unset",left:"25%", background: "red",zIndex:"2"}}
+      onClick={onClick}
+    />
+  );
+}
+
+
+export default function Stories({ windowWidth }) {
   let newsCardArray = [];
   content.forEach((obj, i) =>
     newsCardArray.push(
@@ -66,35 +43,53 @@ export default function Stories({windowWidth}) {
     )
   );
 
-
   
+  const settings = {
+    dots: true,
+    infinite:true,
+    centerMode:true,
+    initialSlide:0,
+    swipeToSlide: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    appendDots: dots => (
+      <div
+        style={{
+        }}
+      >
+        <ul style={{ marginBottom: "0px" }}> {dots} </ul>
+      </div>
+    ),
+    responsive: [
+      {
+        breakpoint: 1250,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
 
   return (
     <div className="stories__wrapper">
       <section className="stories__container">
-        {windowWidth < 950 ? (
-          <Carousel
-            autoPlay={false}
-            axis="horizontal"
-            dynamicHeight={false}
-            centerMode={true}
-            showThumbs={false}
-          >
-            {newsCardArray.map((card) => card)}
-            <div className="stories_news-cards social">
-              <p>Follow us on Social Media</p>
-              <Social fill="#FFFFFF" />
-            </div>
-          </Carousel>
-        ) : (
-          <>
-            {newsCardArray.map((card) => card)}
-            <div className="stories_news-cards social">
-              <p>Follow us on Social Media</p>
-              <Social fill="#FFFFFF" />
-            </div>
-          </>
-        )}
+      <Slider {...settings}>
+        {newsCardArray.map((card) => <div className = "slider_card_container">{card}</div>)}
+        <div className = "slider_card_container">
+        <div className="stories_news-cards social">
+          <p>Follow us on Social Media</p>
+          <Social fill="#FFFFFF" />
+        </div>
+        </div>
+        </Slider>
       </section>
       <a
         href="https://www.ab-inbev.com/news-media/"
---
+++ src/Components/Stories/StoriesContent.js
@@ -0,0 +1,50 @@
+export const content = [
+    {
+      title: "INNOVATION",
+      image:
+        "https://www.ab-inbev.com/content/dam/universaltemplate/ab-inbev/News/Stories/esports-how-ab-inbev-is-capturing-one-of-the-worlds-fastest-growing-entertainment-segments/ESports_427x248.jpg",
+      snippet:
+        "Esports: How AB InBev is Capturing One of the World’s Fastest Growing Entertainment Segments",
+      alt: "Sports Drinks",
+      href: "#",
+      date: "September 07, 2021",
+    },
+    {
+      title: "WATER STEWARDSHIP",
+      image:
+        "https://www.ab-inbev.com/content/dam/universaltemplate/ab-inbev/News/Stories/suma-nativas-working-together-we-can-restore-what-was-destroyed/SumaNativas_427x248.jpg",
+      snippet:
+        "Sumá Nativas: “Working together we can restore what was destroyed”",
+      alt: "Man with plants",
+      href: "#",
+      date: "August 30, 2021",
+    },
+    {
+      title: "BEER & BREWING",
+      image:
+        "	https://www.ab-inbev.com/content/dam/universaltemplate/ab-inbev/News/Stories/bavaria-launches-nativa-a-cassava-based-beer-grown-in-the-colombian-caribbean-/_Nativa_427x248.jpg",
+      snippet:
+        "Bavaria launches Nativa, a cassava-based beer grown in the Colombian Caribbean",
+      alt: "Beer with vegetables in a basket",
+      href: "#",
+      date: "August 23, 2021",
+    },
+    {
+      title: "DREAM PEOPLE CULTURE",
+      image:
+        "https://www.ab-inbev.com/content/dam/universaltemplate/ab-inbev/News/Stories/our-commitment-to-a-better-and-equal-world-for-all--/EqualWorld_427x248.jpg",
+      snippet: "Our Commitment to a Better and Equal World for All",
+      alt: "Our Commitment to a Better and Equal World for All",
+      href: "#",
+      date: "June 23, 2020",
+    },
+    {
+      title: "OUR STORIES",
+      image:
+        "https://www.ab-inbev.com/content/dam/universaltemplate/ab-inbev/News/Stories/our-response-to-covid19-making-an-impact-where-it-matters/OurGlobalResponse_427x248.jpg",
+      snippet: "Our response to COVID19: Making an impact where it matters",
+      alt: "black and white portraits of faces in a grid",
+      href: "#",
+      date: "April 08, 2020",
+    },
+  ];
